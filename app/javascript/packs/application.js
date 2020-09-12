5f1bee1b462d6bc1f6b69a6d59cf1f02709b85be
Jorge Ochoa <jorge.uchija2021@gmail.com>
2020-09-12 16:17:50 -0400

    segundo commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,36 +13,6 @@ import 'css/styles'
 
 import "@fortawesome/fontawesome-free/js/all";
 
-/*
-console.log("si funciona");
-
-var btnAdd = document.getElementById("btnAdd");
-
-var addValueToInput = function () {
-  var inputD = document.getElementById("inputCod");
-  if (!inputD) {
-    console.log("El elemento selecionado no existe");
-  } else {
-    var padre = inputD.parentNode;
-    padre.removeChild(inputD);
-  }
-
-  //<% num_user = rand(1000000000) %>
-  var num = Math.floor((Math.random() * (1000000000-100000000))+100000000);
-  var input = document.createElement("input");
-  var container = document.getElementById("container");
-
-  input.type = "number";
-  input.setAttribute("value", num);
-  input.name = "user[num_user]";
-  input.id = "inputCod";
-  input.className = "d-none"; // set the CSS class
-  container.appendChild(input); // put it into the DOM
-
-  console.log(input);
-};
-btnAdd.addEventListener("click", addValueToInput);
-*/
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
